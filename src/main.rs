244710bdbb3947608f016055eb03e3a2207ddeb9
Carson DeSotel <carson.desotel@gmail.com>
2020-12-19 17:18:57 -0600

    Minor improvements

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ fn read_from_file (filename: &Path) -> Vec<String> {
        .map(|l| l.unwrap())
        .collect()
 }
-
+  
 fn qu_words (lst: &Vec<String>) -> Vec<String> {
     let mut res: Vec<String> = Vec::new();
     for line in lst {
@@ -23,19 +23,41 @@ fn qu_words (lst: &Vec<String>) -> Vec<String> {
     }
     res
 }
-
+  
 fn explode (s: &String) -> Vec<char> {
     s.chars().collect()
 }
-
+  
 fn implode (c: Vec<char>) -> String {
     c.iter().collect()
 }
-
+  
 fn sort (s: &String) -> String {
     let mut r = explode(s);
     r.sort_by(|a, b| a.cmp(b));
     implode(r)
+}
+  
+fn qu_quiz (lst: Vec<String>) -> () {
+    fn helper (s: &String, qu_lst: &Vec<String>, acc: &mut Vec<(String, String)>) {
+        for q in qu_lst {
+            if sort(&q) == sort(&(s.to_owned() + "qu")) {
+                acc.push((s.to_string(), q.to_string()));
+            }
+        }
+    }
+
+    let qu = qu_words(&lst);
+
+    let mut res = Vec::new();
+    for s in lst {
+        helper(&s, &qu, &mut res);
+    }
+    
+    for (s1, s2) in res {
+      println!("({}, {})", s1, s2);
+    }
+    println!("Finished...");
 }
 
 fn main () {
@@ -44,22 +66,5 @@ fn main () {
     let filename = Path::new(&args[1]);
     let lst = read_from_file(filename);
 
-    let qu_words = qu_words(&lst);
-    let qu = "qu".to_string();
-    let mut res: Vec<(String, String)> = Vec::new();
-
-    for s in &lst {
-        let t = format!("{}{}", &qu, &s); 
-        for q in &qu_words {
-            if sort(&q) == sort(&t) {
-                res.push((s.to_string(), q.to_string()));
-            }
-        }
-    }
-
-    for (s1, s2) in res {
-        println!("({}, {})", s1, s2);
-    }
-
-    println!("Finished");
+    qu_quiz(lst);
 }
